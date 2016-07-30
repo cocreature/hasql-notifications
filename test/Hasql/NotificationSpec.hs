0f58ad92eb736f4467c7b128e10fc70ea73a75b2
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2016-07-30 22:03:47 +0200

    Add custom exec function

--- test/Hasql/NotificationSpec.hs
+++ test/Hasql/NotificationSpec.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PartialTypeSignatures #-}
 module Hasql.NotificationSpec
   ( spec
   ) where
@@ -10,12 +11,13 @@ import           Data.Maybe
 import           Data.Text.Encoding (encodeUtf8)
 import qualified Database.PostgreSQL.LibPQ as PQ
 import           Database.PostgreSQL.Simple hiding (execute)
-import           Database.PostgreSQL.Simple.Internal
+import           Database.PostgreSQL.Simple.Internal hiding (exec)
 import           Database.PostgreSQL.Tmp
 import           Hasql.Connection
 import           Hasql.Notification
 import           Hasql.Session
 import           Test.Hspec
+
 
 
 -- withTmpDBConn :: (Connection -> IO a) -> IO a
@@ -29,7 +31,7 @@ import           Test.Hspec
 
 getNotNonBlock = getNotificationNonBlocking' withConn
 
-withTmpDBConn f = 
+withTmpDBConn f =
   bracket (fmap (\(Right conn) -> conn) $ acquire (settings "" 0 "postgres" "" "postgres"))
   release
   f
@@ -70,11 +72,9 @@ spec =
                       takeMVar lock
                       putStrLn "got lock"
                       threadDelay 2000000
-                      res <- execute conn "NOTIFY channel"
+                      res <- withConn conn (\c -> exec c "NOTIFY channel")
                       print res
                       putStrLn "notified"
-                      -- withLibPQConnection conn PQ.flush
-                      putStrLn "flushed"
                       pure ()
                  notification <-
                    getNotification' (\c f -> withConn c (test lock f))
@@ -88,3 +88,43 @@ test lock f c = do putStrLn "filling lock"
                    x <- f c
                    putStrLn "performed op on libpq conn"
                    pure x
+
+-- For some reason it seems to be important that the notify message is
+-- send using this function rather than simply combining sendQuery w
+-- ith getResult like hasql does. Otherwise random race conditions
+-- occur in which threadWaitRead deadlocks.
+exec :: PQ.Connection
+     -> _
+     -> IO PQ.Result
+exec h sql =
+  do success <- PQ.sendQuery h sql
+     if success
+        then awaitResult h Nothing
+        else error "PQsendQuery failed"
+  where awaitResult h mres =
+          do mfd <- PQ.socket h
+             case mfd of
+               Nothing -> error "Database.PostgreSQL.Simple.Internal.exec"
+               Just fd ->
+                 do threadWaitRead fd
+                    _ <- PQ.consumeInput h  -- FIXME?
+                    getResult h mres
+        getResult h mres =
+          do isBusy <- PQ.isBusy h
+             if isBusy
+                then awaitResult h mres
+                else do mres' <- PQ.getResult h
+                        case mres' of
+                          Nothing ->
+                            case mres of
+                              Nothing ->
+                                error "PQgetResult returned no results"
+                              Just res -> return res
+                          Just res ->
+                            do status <- PQ.resultStatus res
+                               case status of
+                                 PQ.CopyOut -> return res
+                                 PQ.CopyIn -> return res
+                                 _ -> error (show status)
+
+-- exec h sql = PQ.sendQuery h sql >> PQ.getResult h
