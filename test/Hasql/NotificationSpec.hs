7133cc8128f5285b6b9fa52081c118cda17a6a85
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2016-08-02 15:02:10 +0200

    Drop dependency on postgres-tmp
    It is not possibly to have finalizers that are always run in hspec as
    far as I can tell. The reason for this is that the threads with the
    finalizers die immediately when the main thread dies and afaik there is
    no way to get hspec to wait for the threads to terminate.

--- test/Hasql/NotificationSpec.hs
+++ test/Hasql/NotificationSpec.hs
@@ -12,7 +12,6 @@ import           Data.Text.Encoding (encodeUtf8)
 import qualified Database.PostgreSQL.LibPQ as PQ
 import           Database.PostgreSQL.Simple hiding (execute)
 import           Database.PostgreSQL.Simple.Internal hiding (exec)
-import           Database.PostgreSQL.Tmp
 import           Hasql.Connection
 import           Hasql.Notification
 import           Hasql.Session
@@ -20,47 +19,45 @@ import           Test.Hspec
 
 
 
--- withTmpDBConn :: (Connection -> IO a) -> IO a
--- withTmpDBConn f =
---   withTmpDB $ \info@(DBInfo {dbName = db, roleName = role}) ->
---     print info >>
---     bracket (fmap (\(Right conn) -> conn) $ acquire (settings "" 0 (encodeUtf8 role) "" (encodeUtf8 db)))
---             release
---             f
+-- withDBConn :: (Connection -> IO a) -> IO a
+withDBConn f =
+  bracket (fmap (\(Right conn) -> conn) $
+           acquire (settings "" 0 "postgres" "" "postgres"))
+          release
+          f
 
 
 getNotNonBlock = getNotificationNonBlocking' withConn
 
-withTmpDBConn f =
-  bracket (fmap (\(Right conn) -> conn) $ acquire (settings "" 0 "postgres" "" "postgres"))
-  release
-  f
+-- withDBConn f =
+--   bracket (fmap (\(Right conn) -> conn) $ acquire (settings "" 0 "postgres" "" "postgres"))
+--   release
+--   f
 withConn = withLibPQConnection
 execute conn t = run (sql t) conn
 
--- withTmpDBConn f = bracket (connectPostgreSQL "user='postgres'") close f
+-- withDBConn f = bracket (connectPostgreSQL "user='postgres'") close f
 -- withConn = withMVar . connectionHandle
 -- execute = execute_
 
 spec :: Spec
 spec =
-  do describe "getNotificationNonBlocking" $
+  do around withDBConn $
+       describe "getNotificationNonBlocking" $
        do it "should return a notification" $
-            withTmpDBConn $
             \conn ->
               do _ <- execute conn "LISTEN channel"
                  _ <- execute conn "NOTIFY channel"
                  notification <- getNotNonBlock conn
                  notification `shouldSatisfy` isJust
           it "should not return a notification" $
-            withTmpDBConn $
             \conn ->
               do _ <- execute conn "LISTEN channel"
                  notification <- getNotNonBlock conn
                  notification `shouldSatisfy` isNothing
-     describe "getNotification'" $
+     around withDBConn $
+       describe "getNotification'" $
        do it "should return a notification" $
-            withTmpDBConn $
             \conn ->
               do
                  -- The lock ensures that getNotification can’t block the connection while it is waiting
